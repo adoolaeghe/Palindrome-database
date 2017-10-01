42e28ba814bd7d45da04521ba6d40bdda9af4b69
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-10-01 09:10:35 +0200

    add get route

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var path = require('path');
 
-
 var bodyParser = require('body-parser');
 
 var app = express();
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', function (req,res){
+  res.render('index');
+});
 
 module.exports = app;
