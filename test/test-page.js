8d05c35596f2c4545c01c4a594c5c6041fcdc9e5
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-10-01 12:04:07 +0200

    add post route storing palindromes to local array

--- test/test-page.js
+++ test/test-page.js
@@ -1,8 +1,25 @@
 var expect  = require('chai').expect;
-var request = require('request');
+var request = require('superagent');
+var app = require('./app');
+var palindrome = 'ollo';
+describe('Status and content', function() {
+  it('Main page content', function() {
+      request('http://localhost:3000' , function(error, response, body) {
+          expect(body).to.equal('Hello World');
+      });
+  });
 
-it('Main page content', function() {
-    request('http://localhost:3000' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
+  it('Main page status', function() {
+      request('http://localhost:3000' , function(error, response, body) {
+          expect(response.statusCode).to.equal(200);
+      });
+  });
+
+  describe ('Palindromes page', function() {
+        it('status', function(){
+            request('http://localhost:3000/palindromes', function(error, response, body) {
+                expect(response.statusCode).to.equal(404);
+            });
+        });
     });
 });
